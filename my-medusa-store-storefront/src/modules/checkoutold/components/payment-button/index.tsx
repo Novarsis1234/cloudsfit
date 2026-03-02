8a89ef73ffedebcf5f58f42d39c923f0cae78323
PrashansaNovarsis <prashansa.jamgade@novarsistech.com>
2026-03-02 17:23:33 +0530

    Fix condition to check cart email and shipping methods

--- my-medusa-store-storefront/src/modules/checkoutold/components/payment-button/index.tsx
+++ my-medusa-store-storefront/src/modules/checkoutold/components/payment-button/index.tsx
@@ -29,7 +29,8 @@ const PaymentButton: React.FC<PaymentButtonProps> = ({
     !cart ||
     !cart.shipping_address ||
     !cart.billing_address ||
-    !cart.email
+    !cart.email ||
+    (cart.shipping_methods?.length ?? 0) < 1
 
   const paymentSession = cart.payment_collection?.payment_sessions?.[0]
 
